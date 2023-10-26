f1b14e58fa9f9de34fa643d22ff851cdf6fa002d
Neider Ruiz <neiderbtc@gmail.com>
2023-10-26 13:43:03 -0500

    add optional param

--- dist/react/hooks/use-translation.d.ts
+++ dist/react/hooks/use-translation.d.ts
@@ -2,6 +2,6 @@ type FlattenKeys<T> = T extends object ? {
     [K in keyof T & string]: T[K] extends string ? K : `${K}.${FlattenKeys<T[K]>}`;
 }[keyof T & string] : '';
 export declare function useTypedTranslation<T>(): {
-    t: (key: FlattenKeys<T>, args: string[]) => any;
+    t: (key: FlattenKeys<T>, args?: string[]) => any;
 };
 export {};
--- src/react/hooks/use-translation.tsx
+++ src/react/hooks/use-translation.tsx
@@ -11,5 +11,5 @@ export function useTypedTranslation<T>() {
   
   const { t } = translation();
 
-  return { t: (key: FlattenKeys<T>, args: string[]) => t(key, args as any) };
+  return { t: (key: FlattenKeys<T>, args?: string[]) => t(key, args as any) };
 }
