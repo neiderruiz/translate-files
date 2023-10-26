df6ab640cb8ae8604ab89a1d571d4e3d2cef34c2
Neider Ruiz <neiderbtc@gmail.com>
2023-10-26 12:49:16 -0500

    clear data before save csv

--- src/translate/utils/translate-file-csv.ts
+++ src/translate/utils/translate-file-csv.ts
@@ -7,18 +7,37 @@ export type ConfigOptions = {
 }
 
 export const translateFileCsv = async (idDoc: string, folderSave: string, config?: ConfigOptions) => {
+  console.log('💊 start load data \n')
+  console.log('🟡 loading... \n')
   fetch(
     `https://docs.google.com/spreadsheets/d/${idDoc}/gviz/tq?tqx=out:csv`
   ).then(async (response) => {
     const data = await response.text();
+    console.log('✅ finish load data \n');
     if (response.status === 200) {
+      console.log('✅ start clear headers empty \n');
+      let dataSave = '';
+      let columnsNotEmpty = 0;
+      data.split('\n').forEach((line) => {
+        const lineSplit = line.split(',');
+        if(!columnsNotEmpty){
+          columnsNotEmpty = lineSplit.filter((column) => column != '""').length;
+        }
+        const firstFive = lineSplit.slice(0, columnsNotEmpty);
+        const joinedFirstFive = firstFive.join(',');
+        dataSave += `${joinedFirstFive}\n`;
+      });
+      console.log('✅ clear headers empty finish \n');
       try {
         if (!fs.existsSync(folderSave)) {
+          console.log('📦 create folder \n')
           fs.mkdirSync(folderSave, { recursive: true });
         }
-        fs.writeFileSync(`${folderSave}/translations-app.csv`, data);
+        
+        console.log(`🤓 write file ${folderSave}/translations-app.csv \n`)
+        fs.writeFileSync(`${folderSave}/translations-app.csv`, dataSave);
       } catch (e: any) {
-        console.error(`Error al escribir el archivo o crear el directorio: ${e.message}`);
+        console.error(`🛑 Error al escribir el archivo o crear el directorio: ${e.message} \n`);
         return;
       }
 
@@ -28,7 +47,7 @@ export const translateFileCsv = async (idDoc: string, folderSave: string, config
           saveWithLevels(jsonObj, folderSave, config);
         });
     } else {
-      console.log("error al obtener los datos");
+      console.log("🛑  error al obtener los datos \n");
     }
   });
 };
