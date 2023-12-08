1a4288131385cf5be77078ee54beb6e64c80e025
Neider Ruiz <neiderbtc@gmail.com>
2023-12-08 12:28:40 -0500

    Refactor translation and renaming logic

--- src/astro/index.ts
+++ src/astro/index.ts
@@ -1,37 +1,42 @@
-import { translateFileCsv } from "../translate/utils/translate-file-csv";
-import path from "path";
 import fs from "fs";
+import path from "path";
+import { langs } from "../translate/types/langs";
+import { translateFileCsv } from "../translate/utils/translate-file-csv";
 
 const renameFiles = (language: string) => {
     const localesPath = './public/locales'
     const outputDir = path.join(localesPath, language);
-    const translatedFilePath = path.join(localesPath,`${language}.json`);
+    const translatedFilePath = path.join(localesPath, `${language}.json`);
     const newFilePath = path.join(localesPath, language, 'translation.json');
     if (!fs.existsSync(outputDir)) {
         fs.mkdirSync(outputDir, { recursive: true });
     }
     if (fs.existsSync(translatedFilePath)) {
         fs.renameSync(translatedFilePath, newFilePath);
-        console.log(`Archivo ${language}.json traducido y renombrado a translation.json`);
+        console.log(`✅ Archivo ${language}.json translated and renamed to translation.json \n`);
     } else {
-        console.log(`Archivo ${language}.json no encontrado`);
+        console.log(`🛑 Archivo ${language}.json not found`);
     }
 }
 
- const translateAndRename = async () => {
-    const localesPath = './public/locales';
+export type ConfigOptionsAstro = {
+    separator?: string;
+}
 
-    await translateFileCsv('your-id', localesPath, {
-        separator: '.',
+const makeTranslations = async (idDoc: string, config?: ConfigOptionsAstro) => {
+    const localesPath = './public/locales';
+    await translateFileCsv(idDoc, localesPath, config);
+    const files = fs.readdirSync(localesPath);
+    const jsonFiles = files.filter(file => path.extname(file) === '.json')
+        .map(file => file.replace('.json', ''));
+    const existingCodes = jsonFiles.filter(code => langs.some(lang => lang.code === code));
+
+    existingCodes.forEach((element) => {
+        renameFiles(element);
     });
-    setTimeout(( ) => {
-        ['es', 'en'].forEach((element) => {
-            renameFiles(element);
-        });
-    }, 2000);
 
 };
 
-export default {
-    translateAndRename
-}
+export {
+    makeTranslations
+};
