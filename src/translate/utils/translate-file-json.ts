810bb4f29c073472ddb449a8add01e8883dba0f1
Neider Ruiz <neiderbtc@gmail.com>
2025-01-14 10:23:57 -0500

    implement function getTranslationsFromAPI

--- src/translate/utils/translate-file-json.ts
+++ src/translate/utils/translate-file-json.ts
@@ -1,17 +1,23 @@
-import EventSource from 'eventsource';
 import * as fs from "fs";
+import { getTranslationsFromAPI, Typeproject } from 'src/translate/utils/get-translations-api';
 import { TypeListLang } from '../types/langs';
 
 export type ConfigOptions = {
   input: TypeListLang;
-  outputs: TypeListLang[];
+  target_langs: TypeListLang[];
+  api_key?: string;
+  typeProject: Typeproject
 }
 
-const countTranslations = (obj: { [key: string]: string | object }): number => {
+export type JsonBase = {
+  [key: string]: string | object;
+}
+
+const countTranslations = (obj: JsonBase): number => {
   let count = 0;
   for (const key in obj) {
     if (typeof obj[key] === 'object') {
-      count += countTranslations(obj[key] as { [key: string]: string | object });
+      count += countTranslations(obj[key] as JsonBase);
     } else {
       count += 1;
     }
@@ -19,44 +25,24 @@ const countTranslations = (obj: { [key: string]: string | object }): number => {
   return count;
 };
 
-export const translateFileJson = async (jsonBase: { [key: string]: string | object }, folderSave: string, config?: ConfigOptions) => {
+export const translateFileJson = async (jsonBase: JsonBase, folderSave: string, config: ConfigOptions) => {
   console.log('💊 start load data \n');
   try {
-    const totalTranslations = config?.outputs?.length || 0;
+    const totalTranslations = config?.target_langs?.length || 0;
     console.log(`🔄 Total translations to perform: ${totalTranslations} \n`);
 
     const totalEntries = countTranslations(jsonBase);
     console.log(`🔄 Total entries to translate in ${config?.input}.json: ${totalEntries} \n`);
 
-    for (const output of config?.outputs || []) {
+    for (const output of config?.target_langs || []) {
       console.log(`✅ running translations ${output} ... \n`);
 
-      const eventSource = new EventSource(`https://translate-files-api-production.up.railway.app/progress`);
-
-      eventSource.onmessage = function (event) {
-        console.log(`${event.data} \n`);
-      };
-
-      eventSource.onerror = function (event) {
-        console.error("Error receiving updates.", event);
-        eventSource.close();
-      };
-
-      const response = await fetch(`https://translate-files-api-production.up.railway.app/traducir?source_lang=${config?.input}&target_lang=${output}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonBase)
-      });
-
-      eventSource.close();
-
-      if (response.status !== 200) {
-        throw new Error(`Failed to fetch data. Status: ${response.status}`);
-      }
-
-      const data = await response.json();
+      const data = await getTranslationsFromAPI({
+        data: jsonBase,
+        sourceLang: config?.input,
+        targetLang: output,
+        typeProject: config?.typeProject,
+      })
 
       if (!fs.existsSync(folderSave)) {
         console.log('📦 create folder \n');
